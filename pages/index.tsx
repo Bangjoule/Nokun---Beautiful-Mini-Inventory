8686b069ab5c0702847a964d341f3ae0226b2629
Bangjoule <holainvestguide@gmail.com>
2022-11-27 20:50:42 +0700

    Day-1

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Nokun</title>
+        <meta name="description" content="Beautiful Inventory Management System" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+       
+      </main>
+
+     
+    </div>
+  )
+}
